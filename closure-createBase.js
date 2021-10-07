7be32941e92e5eabe6cb9caa802547aead59ded7
Souravendra Krishna Deb <souravendra.official@gmail.com>
2021-10-07 22:04:37 +0530

    ll and closure

---
+++ closure-createBase.js
@@ -0,0 +1,17 @@
+/*
+given: var addSix = createBase(6)
+addSix(10) should return 16
+addSix(21) should return 27
+*/
+
+createBase = (number) => {
+    return (result) => {
+        return result + number;
+    }
+}
+
+
+var addSix = createBase(6);
+
+console.warn(addSix(10))
+console.warn(addSix(21))
--- linked-list.js
+++ linked-list.js
@@ -15,5 +15,58 @@ n1.next = n2;
 console.log(n1);
 */
 
-//linked list:
+//Node constructor:
 
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+const n1 = new Node(100, 123);
+//console.log(n1)
+
+//Linked List constructor
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Insert first node (head)
+    insertFirst(data) {
+        this.head = new Node(data, this.head); //sets node as the new head, and sets previous head as this.next
+    }
+
+    //Insert last node
+
+    //Insert at an index
+
+    //Get at index
+
+    //Remove at index
+
+    //Clear list
+
+    //Print list data
+    printListData() {
+        let current = this.head;
+
+        while(current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300)
+
+// console.log(ll)
+
+ll.printListData();
